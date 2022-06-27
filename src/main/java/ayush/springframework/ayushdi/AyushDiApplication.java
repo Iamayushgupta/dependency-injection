e3cc7b1a1c7ef7b84c01540a2694541214e192ea
Ayush <lionelayush446@gmail.com>
2022-06-28 00:27:44 +0530

    Spring Bean Life Cycle Code inserted.

--- src/main/java/ayush/springframework/ayushdi/AyushDiApplication.java
+++ src/main/java/ayush/springframework/ayushdi/AyushDiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-//Solving Assignment Question : implementation of new service(Pets)
+
 @SpringBootApplication
 public class AyushDiApplication {
 
